be50656a4b58c4128fe4497d6d0c4cd01e17c749
Diego Gaspar <diegogaspardacruz1@gmail.com>
2020-03-11 18:05:15 -0300

    Projeto stock-trader adicionado

---
+++ stock-trader/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  
+  <v-toolbar app>
+    <v-toolbar-tittle class="headline text-uppercase mr-4">
+      <span> Stock </span>
+      <span class="font-weight-light"> Trader </span>
+    </v-toolbar-tittle> 
+    <v-toolbar-items>
+      <v-btn flat to="/"> Início </v-btn>
+      <v-btn flat to="/portfolio"> Portfólio </v-btn>
+      <v-btn flat to="/stocks"> Ações </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
