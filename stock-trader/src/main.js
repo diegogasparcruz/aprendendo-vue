f38e3ff2a723a002c19748ab9f677852f99b3e25
Diego Gaspar <diegogaspardacruz1@gmail.com>
2020-03-16 15:07:03 -0300

    Configurando Axios & Firebase

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import './plugins/vuetify'
 import App from './App.vue'
 
 import store from './store/store'
 import router from './router'
+
+import './plugins/vuetify'
+import './plugins/axios'
 
 Vue.config.productionTip = false
 
